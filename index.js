5362ea05e89d6849ce3ccf523bb1aa2c8151e6b0
Florenzo <florenzoraphael2005@gmail.com>
2023-10-13 13:28:23 +0800

    enoko 2hu

---
+++ index.js
@@ -0,0 +1,33 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(8080);
+
+// *** GET Routes - display pages ***
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.get('/about', function (req, res) {
+    res.render('pages/about');
+});
+
+app.get('/contact', function (req, res) {
+    res.render('pages/contact');
+});
+
+app.get('/product-2', function (req, res) {
+    res.render('pages/product-2');
+});
+
+app.get('/projects', function (req, res) {
+    res.render('pages/projects');
+});
